7ccb9146f53263190c4466697a09a108887fc480
peddisuchitha <peddisuchitha03@gmail.com>
2025-09-03 17:48:25 +0530

    Updated components and fixed bugs

--- src/main.ts
+++ src/main.ts
@@ -14,12 +14,12 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://react.dev/learn" target="_blank">
   <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" class="logo vanilla" alt="React logo" />
 </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Vite + TypeScript + React</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+      Click on the Vite,TypeScript and React logos to learn more
     </p>
   </div>
 `
